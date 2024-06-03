f982aca83a7fb77b893469029624a33798d43754
Irmak Ege <irmakege5@gmail.com>
2024-06-03 19:34:05 +0300

    main section created

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,26 @@ import styles from './App.module.css';
 export function App() {
   return (
     <>
-      <header>
+      {/* <header>
         <h1>HubX Frontend Assignment</h1>
-      </header>
+        
+      </header> */}
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <section className='subcontainer'>
+          <div className="subsubcontainer1">
+            <h1>IMAGE</h1>
+          </div>
+          <div className="subsubcontainer2">
+            <h1>Text</h1>
+          </div>
+        </section>
+        <section>
+          <div>
+
+          </div>
+        </section>
+      </main>
     </>
   );
 }
